07de876fcd32263d3a5e2f3091005aad7b0551cb
Doron Segal <doron2402@gmail>
2015-10-28 23:58:21 -0700

    starting

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # vast-api
 vast api
+
+Listen to port 3000
+
+To add new xml just add new file to `xmls` directory
+
+## End-Points
+- [GET] /api/vast
--- index.js
+++ index.js
@@ -1,9 +1,13 @@
-var express = require('../../');
-
+var express = require('express');
 var app = express();
+var fs = require('fs');
+var XML_PATH = './xmls';
 
 app.get('/api/vast', function(req, res){
-    
+  var xmls = fs.readdirSync(XML_PATH);   
+  var xml = xmls[Math.floor(Math.random()*xmls.length)];
+  var file = fs.readFileSync(XML_PATH + '/' + xml);
+  res.set('Content-Type', 'text/xml').send(file);
 });
 
 /* istanbul ignore next */
